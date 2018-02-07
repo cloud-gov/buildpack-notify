98e92e550c4ceba01ee5c2ab8b134969d2cc11a7
Will Woodson <william.woodson@gsa.gov>
2018-02-06 17:48:02 -0700

    update tasks

--- ci/notify.sh
+++ ci/notify.sh
@@ -5,7 +5,13 @@ set -e -x
 export GOPATH=$(pwd)/gopath
 export PATH=$PATH:$GOPATH/bin
 
+export IN_STATE="$(pwd)/${IN_STATE}"
+export OUT_STATE="$(pwd)/${OUT_STATE}"
+
+go get -u github.com/golang/dep/cmd/dep
+
 cd gopath/src/github.com/18F/cg-buildpack-notify
+dep ensure
 go build
 
 ./cg-buildpack-notify
