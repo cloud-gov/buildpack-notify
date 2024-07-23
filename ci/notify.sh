0117a38dbf436b86e8bd3e52f3cc8d6dbde6af53
Mark Boyd <mark.boyd@gsa.gov>
2024-07-23 11:46:00 -0400

    apply shellcheck suggestions to scripts

--- ci/notify.sh
+++ ci/notify.sh
@@ -2,17 +2,18 @@
 
 set -ex
 
-export GOPATH=$(pwd)/gopath
+GOPATH=$(pwd)/gopath
+export GOPATH
 export PATH=$PATH:$GOPATH/bin
-mkdir -p ${GOPATH}/bin
+mkdir -p "${GOPATH}/bin"
 
-export IN_STATE="$(pwd)/${IN_STATE}"
-export OUT_STATE="$(pwd)/${OUT_STATE}"
+IN_STATE="$(pwd)/${IN_STATE}"
+OUT_STATE="$(pwd)/${OUT_STATE}"
 
-curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+export IN_STATE
+export OUT_STATE
 
 pushd gopath/src/github.com/cloud-gov/cg-buildpack-notify
-  dep ensure
   go mod vendor
   go build
   ./cg-buildpack-notify
